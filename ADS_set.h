85e968447c69b08e8c60c4749e795a0ef05715b8
Romanov Vitalii <a01549702@unet.univie.ac.at>
2023-04-30 22:52:52 +0200

    Minor improvements

--- ADS_set.h
+++ ADS_set.h
@@ -24,27 +24,31 @@ public:
   using key_equal = std::equal_to<key_type>; // Hashing
   using hasher = std::hash<key_type>;        // Hashing
 private:
-  struct Bucket{
+  struct Bucket
+  {
     Key value;
     Bucket *next = nullptr;
-    ~Bucket(){
+    ~Bucket()
+    {
       delete next;
     }
   };
 
-  struct Table{
+  struct Table
+  {
     Bucket *head = nullptr;
-    ~Table(){
+    ~Table()
+    {
       delete head;
     }
   };
 
   size_t sz = 0;
-  size_t tablesize = 0;
   size_t max_sz{N};
   Table *table{nullptr};
   size_type hash_idx(const key_type &k) const { return hasher{}(k) % max_sz; }
-  void insert_unchecked(const key_type &k){
+  void insert_unchecked(const key_type &k)
+  {
     size_type hashwert = hash_idx(k);
     Bucket *help;
     help = new Bucket();
@@ -58,12 +62,15 @@ private:
       rehash();
   }
 
-  bool find_pos(const key_type &k) const{
+  bool find_pos(const key_type &k) const
+  {
     bool result = false;
     size_type hashwert = hash_idx(k);
-    if (table[hashwert].head != nullptr){
+    if (table[hashwert].head != nullptr)
+    {
       auto current = table[hashwert].head;
-      do{
+      do
+      {
         if (key_equal{}(current->value, k))
           result = true;
         current = current->next;
@@ -71,17 +78,20 @@ private:
       current = nullptr;
       delete current;
     }
-
     return result;
   }
 
-  Bucket *find_pos2(const key_type &k) const{
+  Bucket *find_pos2(const key_type &k) const
+  {
     Bucket *pos = nullptr;
     size_type hashwert = hash_idx(k);
-    if (table[hashwert].head != nullptr){
+    if (table[hashwert].head != nullptr)
+    {
       auto current = table[hashwert].head;
-      do{
-        if (key_equal{}(current->value, k)){
+      do
+      {
+        if (key_equal{}(current->value, k))
+        {
           pos = current;
           return pos;
         }
@@ -92,12 +102,16 @@ private:
     return pos;
   }
 
-  void rehash(){
+  void rehash()
+  {
     std::vector<Key> temp;
-    for (int i = 0; i < max_sz; i++){
-      if (table[i].head != nullptr){
+    for (int i = 0; i < max_sz; i++)
+    {
+      if (table[i].head != nullptr)
+      {
         auto current = table[i].head;
-        do{
+        do
+        {
           temp.push_back(current->value);
           current = current->next;
         } while (current != nullptr);
@@ -114,38 +128,26 @@ private:
   }
 
 public:
-  ADS_set(){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
-  }
-  ADS_set(std::initializer_list<key_type> ilist){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
+  ADS_set() : table{new Table[N]}, max_sz{N} {}
+
+  ADS_set(std::initializer_list<key_type> ilist) : ADS_set()
+  {
     insert(ilist);
   }
   template <typename InputIt>
-  ADS_set(InputIt first, InputIt last){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
+  ADS_set(InputIt first, InputIt last) : ADS_set()
+  {
     insert(first, last);
   }
-  ADS_set(const ADS_set &other){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
+  ADS_set(const ADS_set &other) : ADS_set()
+  {
     for (const auto &k : other)
       insert_unchecked(k);
   }
   ~ADS_set() { delete[] table; }
 
-  ADS_set &operator=(const ADS_set &other){
+  ADS_set &operator=(const ADS_set &other)
+  {
     if (this == &other)
       return *this;
     clear();
@@ -154,56 +156,60 @@ public:
     return *this;
   }
 
-  ADS_set &operator=(std::initializer_list<key_type> ilist){
+  ADS_set &operator=(std::initializer_list<key_type> ilist)
+  {
     clear();
     insert(ilist);
     return *this;
   }
 
-  size_type size() const{
+  size_type size() const
+  {
     return sz;
   }
 
-  bool empty() const{
+  bool empty() const
+  {
     return !sz;
   }
 
-  size_type count(const key_type &key) const{
+  size_type count(const key_type &key) const
+  {
     return find_pos(key);
   }
 
-  iterator find(const key_type &key) const{
+  iterator find(const key_type &key) const
+  {
     if (Bucket * pos{find_pos2(key)})
       return const_iterator{pos, (int)hash_idx(key), table, max_sz};
     return end();
   }
 
-  void clear(){
+  void clear()
+  {
     delete[] table;
     sz = 0;
     max_sz = N;
     table = new Table[max_sz];
   }
 
-  void swap(ADS_set &other){
+  void swap(ADS_set &other)
+  {
     using std::swap;
     swap(sz, other.sz);
     swap(max_sz, other.max_sz);
-    swap(tablesize, other.tablesize);
     swap(table, other.table);
   }
 
-  void insert(std::initializer_list<key_type> ilist){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
+  void insert(std::initializer_list<key_type> ilist)
+  {
     for (const auto &k : ilist)
       if (!find_pos(k))
         insert_unchecked(k);
   }
 
-  std::pair<iterator, bool> insert(const key_type &key){
+  std::pair<iterator, bool> insert(const key_type &key)
+  {
     if (find_pos2(key))
       return std::make_pair(const_iterator{find_pos2(key), (int)hash_idx(key), table, max_sz}, false);
 
@@ -212,28 +218,33 @@ public:
   }
 
   template <typename InputIt>
-  void insert(InputIt first, InputIt last){
-    if (tablesize == 0){
-      table = new Table[N];
-      tablesize = N;
-    }
-    for (auto it = first; it != last; ++it){
-      if (!find_pos(*it)){
+  void insert(InputIt first, InputIt last)
+  {
+    for (auto it = first; it != last; ++it)
+    {
+      if (!find_pos(*it))
+      {
         insert_unchecked(*it);
       }
     }
   }
 
-  size_type erase(const key_type &key){
-    if (find_pos(key)){
+  size_type erase(const key_type &key)
+  {
+    if (find_pos(key))
+    {
       size_type hashwert = hash_idx(key);
-      if (table[hashwert].head != nullptr){
+      if (table[hashwert].head != nullptr)
+      {
         Bucket *current = table[hashwert].head;
         Bucket *beforcurrent = table[hashwert].head;
         int counter = 0;
-        do{
-          if (key_equal{}(current->value, key)){
-            if (counter == 0){
+        do
+        {
+          if (key_equal{}(current->value, key))
+          {
+            if (counter == 0)
+            {
               table[hashwert].head = current->next;
               current->next = nullptr;
               delete current;
@@ -242,7 +253,8 @@ public:
               sz--;
               return 1;
             }
-            else{
+            else
+            {
               beforcurrent->next = current->next;
               current->next = nullptr;
               beforcurrent = nullptr;
@@ -261,10 +273,13 @@ public:
     return 0;
   }
 
-  int findfirst() const{
+  int findfirst() const
+  {
     int firstelement = 0;
-    for (size_type i = 0; i < max_sz; i++){
-      if (table[i].head != nullptr){
+    for (size_type i = 0; i < max_sz; i++)
+    {
+      if (table[i].head != nullptr)
+      {
         firstelement = i;
         return firstelement;
       }
@@ -272,32 +287,31 @@ public:
     return firstelement;
   }
 
-  /**
-    int findlast()const {
-    int lastelement=0;
-    for(size_type i=0; i<max_sz; i++){
-      if(table[i].head!=nullptr){lastelement=i;}
-    }
-    return lastelement;
-  } 
-**/
   const_iterator begin() const { return const_iterator{table[this->findfirst()].head, findfirst(), table, max_sz}; }
   const_iterator end() const { return const_iterator{nullptr}; }
 
-  void dump(std::ostream &o = std::cerr) const{
-    for (int i = 0; i < max_sz; i++){
-      if (table[i].head != nullptr){
+  void dump(std::ostream &o = std::cerr) const
+  {
+    o << "current size: " << sz << ", max size: " << max_sz << std::endl;
+    for (int i = 0; i < max_sz; i++)
+    {
+      o << i << ":";
+      if (table[i].head != nullptr)
+      {
         auto current = table[i].head;
-        do{
-          o << i << ": value: " << current->value << std::endl;
+        do
+        {
+          o << " --> " << current->value;
           current = current->next;
         } while (current != nullptr);
         current = nullptr;
         delete current;
       }
+      o << std::endl;
     }
   }
-  friend bool operator==(const ADS_set &lhs, const ADS_set &rhs){
+  friend bool operator==(const ADS_set &lhs, const ADS_set &rhs)
+  {
     if (lhs.sz != rhs.sz)
       return false;
     for (const auto &k : lhs)
@@ -309,7 +323,8 @@ public:
 };
 
 template <typename Key, size_t N>
-class ADS_set<Key, N>::Iterator{
+class ADS_set<Key, N>::Iterator
+{
   Bucket *pos;
   int element;
   Table *table;
@@ -327,7 +342,8 @@ public:
   {
   }
 
-  ~Iterator(){
+  ~Iterator()
+  {
     table = nullptr;
     pos = nullptr;
     delete pos;
@@ -337,18 +353,22 @@ public:
   reference operator*() const { return pos->value; }
   pointer operator->() const { return &pos->value; }
 
-  void skip(){
+  void skip()
+  {
     while (table[element].head == nullptr && element < max_sz)
     {
       element++;
     }
   }
 
-  Iterator &operator++(){
-    if (pos->next != nullptr && pos != nullptr){
+  Iterator &operator++()
+  {
+    if (pos->next != nullptr && pos != nullptr)
+    {
       pos = pos->next;
     }
-    else{
+    else
+    {
       element++;
       skip();
       if (element == max_sz)
@@ -361,7 +381,8 @@ public:
 
     return *this;
   }
-  Iterator operator++(int){
+  Iterator operator++(int)
+  {
     auto rc = *this;
     ++*this;
     return rc;
--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   cout << "Testing program for ADS_Set" << endl;
 
-  ADS_set<double> set1={1,5,34,34.54,2312.5,34535.5};
+  ADS_set<int> set1={1,5,34,34,54};
   set1.dump();
 
 }
